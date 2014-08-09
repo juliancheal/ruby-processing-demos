984d1a4b31a8be52eddd5051327359a4af8e58d6
Julian Cheal <julian.cheal@gmail.com>
2014-08-09 06:48:54 +0300

    Simple video demo

---
+++ video-sample.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+ 
+# this is a test of ruby-processing (https://github.com/jashkenas/ruby-processing) with the video library
+ 
+# use "rp5 unpack library" at a command line to install the video library, among others
+ 
+# tested with Ruby 1.9.2
+# video file: http://bit.ly/H5yBjK
+ 
+class VideoTest < Processing::App
+  load_library :video
+  include_package "processing.video"
+ 
+  def setup
+    size(640, 480, P2D)
+    @movie = Movie.new(self, "data/sample.mov")
+    @movie.play                                                                
+  end
+ 
+  def draw
+    tint(*Array.new(3) { rand(256) })
+    image(@movie, 0,0)
+  end
+ 
+  def movie_event(m)
+    m.read
+  end
+ 
+end
